b43c8fa7669cbb21653b091762460133b77af763
mazhanbaig <mazhanbaig44@gmail.com>
2025-12-04 20:43:33 +0500

    err sol

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import { UserProvider } from "./context/UserContext";
 import 'antd/dist/antd.css';
 
 
@@ -31,7 +30,6 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
-        {/* <Header/> */}
         {children}
       </body>
     </html>
