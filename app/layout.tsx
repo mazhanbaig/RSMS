7e0cb59771cae71dc983f46ca83f9654b26d5f02
mazhanbaig <mazhanbaig44@gmail.com>
2025-12-07 12:02:50 +0500

    err git push

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="googled45320be1a95a971.html" />
+        <meta name="google-site-verification" content="d45320be1a95a971.html" />
       </head>
       <body
         className={`antialiased`}
