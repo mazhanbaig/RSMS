80f12a229b138f8abb5d598a21a3d2924b3d6fd4
mazhanbaig <mazhanbaig44@gmail.com>
2025-12-19 14:50:16 +0500

    sold

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -117,19 +117,20 @@ export default function PropertiesPage() {
                             key={p.id}
                             className="group relative bg-white rounded-xl overflow-hidden shadow-xl hover:shadow-2xl transition-all duration-500"
                         >
-                            {p.dealCompleted && (
-                                <div className="absolute top-3 right-3 z-10">
-                                    <span className="px-3 py-1 text-xs font-bold text-white bg-green-600 rounded-full shadow">
-                                        SOLD
-                                    </span>
-                                </div>
-                            )}
-
                             {/* Content */}
                             <div className="p-5">
                                 <div className="flex justify-between items-start mb-3">
                                     <div className="flex-1">
-                                        <h3 className="text-lg font-bold text-gray-900 line-clamp-1 mb-1">{p.title}</h3>
+                                        <div className="flex gap-2">
+                                            <h3 className="text-lg font-bold text-gray-900 line-clamp-1 mb-1">{p.title}</h3>
+                                            {p.dealCompleted && (
+                                                <div className=" z-10">
+                                                    <span className="px-3 py-1 text-xs font-bold text-white bg-green-600 rounded-full shadow">
+                                                        SOLD
+                                                    </span>
+                                                </div>
+                                            )}
+                                        </div>
                                         <div className="flex items-center gap-1.5 text-gray-600">
                                             <MapPin size={14} className="text-purple-600" />
                                             <span className="text-sm truncate">{p.location}</span>
